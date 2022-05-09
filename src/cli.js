0772a28bb0d552fd03bd9e371d8973f1e12df258
kntowd <kenta2231989@gmail.com>
2022-05-09 20:57:19 +0900

    add option

--- src/cli.js
+++ src/cli.js
@@ -5,14 +5,20 @@ const yargs = require('yargs/yargs')
 yargs(process.argv.slice(2))
   .command({
     command: "hello",
-    handler: (argv) => {
+    handler: () => {
       console.log('hello world!!');
     }
   })
   .command({
     command: "self-introduction <name>",
+    builder: (yargs) => {
+      yargs.option('age',{
+        alias: 'a',
+        demandOption: true,
+      })
+    },
     handler: (argv) => {
-      console.log(`I am ${argv.name}`)
+      console.log(`I am ${argv.name}!! I am ${argv.age} old.`)
     }
   })
   .argv
